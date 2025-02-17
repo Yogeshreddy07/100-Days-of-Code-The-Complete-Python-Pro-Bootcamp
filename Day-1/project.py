829e9bfb377b575b31ece790fd8549cfc7fedf97
Yogesh Reddy <yogiyogeshreddy1234@gmail.com>
2025-02-17 12:03:31 +0530

    added Day-1

---
+++ Day-1/project.py
@@ -0,0 +1,15 @@
+#project 1
+print("Welcome to the Band Name Generator.")
+name_city=input("What's the name of the city you grew up in?\n")
+pet_name=input("What's your pet's name?\n") # \n used for next line which will show in terminal for input
+print(f"Your band name could be {name_city} {pet_name}")
+
+"""
+Output: 
+Welcome to the Band Name Generator.
+What's the name of the city you grew up in? 
+Hosur #input
+What's your pet's name?
+Cat  #input
+Your band name could be Hosur Cat #output
+"""
---
+++ Day-1/task.py
@@ -0,0 +1,28 @@
+#saw basic concepts like print,input , variables in day1 of codeing
+#print function
+print("hello world")
+
+#\n -->used for next line
+print("hello world \nhello world")
+
+#string cancatination
+print("yogesh"+"reddy") #no space
+
+#string cancatination using space
+print("yogesh"+" "+"reddy")
+
+#saw about indentaion error.
+
+#input function -> used to take input from the user
+a=input("What is your name??")
+print("Hello " + a)
+print("Hello " + input("What is your name??"))
+print(f"Hello {a}")
+
+#variables
+name =input("What is your name??")
+print(name)
+name="yogesh reddy"
+print(len(name))
+print(type(name),len(name))
+print(name)
